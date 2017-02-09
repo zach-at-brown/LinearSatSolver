1d040bf1ccedbe3edcb1fc2cb4cb56c2239b235c
Shreyas1707 <=>
2017-02-09 15:33:11 -0500

    Added methods for removing clauses and literals

--- src/solver/sat/Main.java
+++ src/solver/sat/Main.java
@@ -4,11 +4,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
- * Usage example: read a given cnf instance file to create 
- * a simple sat instance object and print out its parameter fields. 
+ * Usage example: read a given cnf instance file to create
+ * a simple sat instance object and print out its parameter fields.
  */
 public class Main
-{  
+{
   public static void main(String[] args) throws Exception
   {
 		if(args.length == 0)
@@ -16,17 +16,18 @@ public class Main
 			System.out.println("Usage: java Main <cnf file>");
 			return;
 		}
-		
+
 		String input = args[0];
 		Path path = Paths.get(input);
 		String filename = path.getFileName().toString();
-    
+
     Timer watch = new Timer();
     watch.start();
-    
+
 		SATInstance instance = DimacsParser.parseCNFFile(input);
+
 		System.out.println(instance);
-    
+
 		watch.stop();
     System.out.println("Instance: " + filename + " Time: " + String.format("%.2f",watch.getTime()) + " Result: ?");
   }
